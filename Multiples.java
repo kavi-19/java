11f2d3b38a025e182a02114ddb1b35b581e76f31
kavi-19 <kavithakaari1924@gmail.com>
2024-07-30 20:27:11 +0530

    initial code

---
+++ Multiples.java
@@ -0,0 +1,20 @@
+
+
+
+public class Multiples {
+  public static void main(String[] args) {
+		int sum =0;
+		int result=0;
+		for(int i=0;i<=100;i++) {
+			if(i%5==0) {
+				sum += i;
+			}
+		}
+		result = sum;
+				System.out.println(result+" Multiples of 5");
+				
+			}
+	
+		}
+  
+
